7aa896ff3f0c9cd4dfd7de2c19a6de45ccbc0016
Surya-github-cloud <suryavamsepdp@gmail.com>
2025-09-14 14:20:15 +0530

    Update init.sh

--- docker/init.sh
+++ docker/init.sh
@@ -6,42 +6,35 @@ if [ -d "/home/frappe/frappe-bench/apps/frappe" ]; then
     bench start
 else
     echo "Creating new bench..."
-    
-    bench init --skip-redis-config-generation frappe-bench
-    cd frappe-bench
-    
-    # Use containers instead of localhost for MariaDB and Redis
-    bench set-mariadb-host mariadb
-    bench set-redis-cache-host redis://redis:6379
-    bench set-redis-queue-host redis://redis:6379
-    bench set-redis-socketio-host redis://redis:6379
-    
-    # Remove redis and watch processes from Procfile to avoid conflicts
-    sed -i '/redis/d' ./Procfile
-    sed -i '/watch/d' ./Procfile
-    
-    # Get ERPNext and HRMS apps
-    bench get-app erpnext
-    bench get-app hrms
-    
-    # Create new site with environment-provided passwords
-    bench new-site hrms.localhost \
-      --force \
-      --mariadb-root-password 123 \
-      --admin-password admin \
-      --no-mariadb-socket
-    
-    # Install HRMS app
-    bench --site hrms.localhost install-app hrms
-    
-    # Enable developer mode and scheduler, clear cache
-    bench --site hrms.localhost set-config developer_mode 1
-    bench --site hrms.localhost enable-scheduler
-    bench --site hrms.localhost clear-cache
-    
-    # Use the new site by default
-    bench use hrms.localhost
-    
-    # Finally start bench
-    bench start
 fi
+
+export PATH="${NVM_DIR}/versions/node/v${NODE_VERSION_DEVELOP}/bin/:${PATH}"
+
+bench init --skip-redis-config-generation frappe-bench
+cd frappe-bench
+
+# Use containers instead of localhost
+bench set-mariadb-host mariadb
+bench set-redis-cache-host redis://redis:6379
+bench set-redis-queue-host redis://redis:6379
+bench set-redis-socketio-host redis://redis:6379
+
+# Remove redis, watch from Procfile
+sed -i '/redis/d' ./Procfile
+sed -i '/watch/d' ./Procfile
+
+bench get-app erpnext
+bench get-app hrms
+
+bench new-site hrms.localhost \
+  --force \
+  --mariadb-root-password 123 \
+  --admin-password admin \
+  --no-mariadb-socket
+
+bench --site hrms.localhost install-app hrms
+bench --site hrms.localhost set-config developer_mode 1
+bench --site hrms.localhost enable-scheduler
+bench --site hrms.localhost clear-cache
+bench use hrms.localhost
+bench start
